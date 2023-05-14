39745df5c2cb282ccfc4f1b41e84fe807ec51737
Ishaan Das <ishaandas@ishaans-mbp.mynetworksettings.com>
2023-05-14 00:55:13 -0400

    sd

---
+++ js/questions.js
@@ -0,0 +1,101 @@
+let questions = [
+  {
+    numb: 1,
+    question: "What should you do after graduating from high school?",
+    answer: "Join the circus and become a clown",
+    options: [
+    "Become an engineer at a big tech company",
+    "Start a goat farm and become a goat farmer",
+    "Join the circus and become a clown",
+    "Become a professional video game player"
+    ]
+    },
+    {
+    numb: 2,
+    question: "What is the best way to find a job?",
+    answer: "Stalk the CEO and convince them to hire you",
+    options: [
+    "Apply to as many jobs as possible",
+    "Create a LinkedIn profile and network with professionals",
+    "Start your own business",
+    "Stalk the CEO and convince them to hire you"
+    ]
+    },
+    {
+    numb: 3,
+    question: "What is the most important factor to consider when choosing a college major?",
+    answer: "Choosing a major that will allow you to time travel",
+    options: [
+    "Choosing a major that aligns with your passion",
+    "Choosing a major that will allow you to make a positive impact on society",
+    "Choosing a major that is easy to get good grades in",
+    "Choosing a major that will allow you to time travel"
+    ]
+    },
+    {
+    numb: 4,
+    question: "What is the best way to become successful?",
+    answer: "Summon a genie and make a wish",
+    options: [
+    "Follow a traditional path and play it safe",
+    "Work hard and save money",
+    "Inherit a large sum of money from your rich uncle",
+    "Summon a genie and make a wish"
+    ]
+    },
+    {
+    numb: 5,
+    question: "What is the best way to stand out in a job interview?",
+    answer: "Perform a magic trick",
+    options: [
+    "Wear a suit and tie",
+    "Bring a resume and references",
+    "Research the company and ask thoughtful questions",
+    "Perform a magic trick"
+    ]
+    },
+    {
+    numb: 6,
+    question: "What is the best way to get fit?",
+    answer: "Do a backflip every morning",
+    options: [
+    "Go for a daily run",
+    "Do yoga and meditation",
+    "Lift weights at the gym",
+    "Do a backflip every morning"
+    ]
+    },
+    {
+    numb: 7,
+    question: "What is the best way to make friends?",
+    answer: "Wear a chicken suit and cluck like a chicken",
+    options: [
+    "Join a club or organization",
+    "Attend social events",
+    "Volunteer for a cause",
+    "Wear a chicken suit and cluck like a chicken"
+    ]
+    },
+    {
+    numb: 8,
+    question: "What is the best way to learn a new language?",
+    answer: "Watch cartoons in that language and mimic the characters",
+    options: [
+    "Take a class at a language school",
+    "Use a language learning app",
+    "Immerse yourself in the culture by traveling",
+    "Watch cartoons in that language and mimic the characters"
+    ]
+    },
+    {
+    numb: 9,
+    question: "What is the best way to become a millionaire?",
+    answer: "Invent a time machine and bet on future sporting events",
+    options: [
+    "Save and invest wisely",
+    "Start your own business",
+    "Win the lottery",
+    "Invent a time machine and bet on future sporting events"
+    ]
+    }
+];
